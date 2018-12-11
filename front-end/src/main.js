a660adb3775a7bffd043f80bcd009a4664650dad
fanke <704317802@qq.com>
2018-12-11 20:57:56 +0800

    '666'

---
+++ front-end/src/main.js
@@ -0,0 +1,18 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import router from './router';
+
+// 去除vue开发时的浏览器提示。
+Vue.config.productionTip = false;
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {
+    App: App
+  },
+  template: '<App/>'
+});
---
+++ front-end/src/router.js
@@ -0,0 +1,10 @@
+// // 路由相关的代码
+// import Vue from 'vue';
+// import VueRouter from 'vue-router';
+
+// // 插件安装
+// Vue.use(VueRouter);
+
+// const router = new VueRouter();
+
+// export default router;
