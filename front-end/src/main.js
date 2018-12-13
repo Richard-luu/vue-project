943aaf19334a9f27e67f02c4cbfdd91d4d389db2
fanke <704317802@qq.com>
2018-12-13 11:13:59 +0800

    12-13

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,5 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+
+// 整个前端项目的入口js文件。
 import Vue from 'vue';
 import App from './App';
 import router from './router';
